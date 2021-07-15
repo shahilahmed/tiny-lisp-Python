7d8c4e072ff6197e34efe4978d7d56d5ca3126ee
Md Shahil Ahmed <54341137+shahilahmed@users.noreply.github.com>
2021-07-15 20:31:19 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,99 @@
+from Core.Lexer  import *
+from Core.Parser import *
+from Core.Evaluator import *
+from datetime import datetime
+
+NAME    = "Tiny Lisp Python"
+VERSION = "v0.6.0"
+AUTHOR  = "Md Shahil Ahmed"
+
+env = Environment()
+env.define("pi",3.14)
+
+def interpret_source(source = ""):
+	if len(source) != 0:
+		tokens = Lexer.lex(source)
+		tree   = Parser(tokens).parse()
+		evaluate(tree,env)
+
+def interpret_file(path = ""):
+	if file_exists(file_path_full(path)):
+		interpret_source(file_get_contents(file_path_full(path)))
+
+def interpret_repl(prompt = "tl>"):
+	source  = ""
+	history = []
+	while True:
+		try:
+			source = source + input(prompt if len(source) == 0 else "... ")
+			if source in ["exit","quit","bye"]:
+				break
+			if source.count('(') != source.count(')'):
+				source = source + ' \n'
+			else:
+				source = source.replace('\n',' ')
+				history.append(source)
+				tokens = Lexer.lex(source)
+				tree   = Parser(tokens).parse()
+				result = evaluate_exp(tree[0],env)
+				if result != None:
+					print(result)
+				source = ""
+		except Exception as e:
+			source = ""
+			print(e)
+		print()
+	contents = "\n".join(history)
+	contents  = ";;Session : {}\n{}\n\n".format(datetime.now(),contents)
+	path = file_path_full("repl\session.tl")
+	if file_exists(path):
+		file_put_contents(path,file_get_contents(path) + contents)		
+	else:
+		file_put_contents(path,contents)		
+
+def print_info():
+	print()
+	print("{} {} {}".format(NAME,VERSION,AUTHOR))
+	print("Copyright {}".format(datetime.now().year))
+	print()
+	print("usage: python main.py")
+	print()
+
+def main():
+	print_info()
+	argv = sys.argv[1:]
+	if len(argv) >= 1:
+		command = argv.pop(0)
+		argv = list(filter(lambda x : file_exists(file_path_full(x)),argv))
+		if command == "-help":	
+			print()
+			print("python main.py -X path\\file_1.ext path\\file_2.ext path\\file_n.ext")
+			print()
+			print("Here -X is given below")
+			print()
+			print(" -h : help")
+			print(" -r : to open REPL(Read-Evaluate-PRINT-LOOP)")
+			print(" -i : interpret one by one file(s)")
+			print(" -l : link all file(s) and then interpret")
+			print()
+		if command == "-r":	
+			interpret_repl()
+		elif command == "-i":	
+			for path in argv:
+				env = Environment()
+				env.define("pi",3.14)
+				interpret_file(path)
+		elif command == "-l":	
+			source = ""
+			for path in argv:
+				source = source + "{}\n".format(file_get_contents(file_path_full(path)))
+			interpret_source(source)
+		else:
+			print("usage: python main.py -help")
+	else:
+		print("usage: python main.py -help")
+	
+if __name__ == "__main__":
+	interpret_file("lib\\lib.tl")
+	main()
+
